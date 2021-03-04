698dc2136f2085ba6d35175abe452103902fbaad
Zialz <loris.alexandre7@etu.univ-lorraine.fr>
2021-03-04 15:16:08 +0100

    Initial commit
    Basic puzzles and interactions / No environnement

---
+++ Assets/SnapToGrid.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SnapToGrid : MonoBehaviour
+{
+    private XRGrabInteractable grabInteractable;
+    public Vector3 gridSize = default;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grabInteractable = GetComponent<XRGrabInteractable>();
+        grabInteractable.onSelectExited.AddListener(Snap);
+    }
+
+    private void Snap(XRBaseInteractor interactor)
+    {
+        var position = new Vector3(
+        Mathf.RoundToInt(this.transform.position.x / this.gridSize.x) * this.gridSize.x,
+        Mathf.RoundToInt(this.transform.position.y / this.gridSize.y) * this.gridSize.y,
+        Mathf.RoundToInt(this.transform.position.z / this.gridSize.z) * this.gridSize.z
+        );
+        this.transform.position = position;
+    }
+}
---
+++ Assets/XRGUI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class XRGUI : MonoBehaviour
+{
+    // UI ELEMENTS
+    public Text text;
+    public Button test;
+    public Button resetPuzzle;
+
+    // RESET MODEL
+    public GameObject model;
+    private Vector3 posModel;
+
+
+    // RESET PUZZLE
+    public GameObject puzzle;
+    private List<GameObject> puzzlePieces = new List<GameObject>();
+    private List<Vector3> pos = new List<Vector3>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // RESET PUZZLE INI
+        GameObject originalGameObject = puzzle;
+        for (int i = 0; i < originalGameObject.transform.childCount; i++)
+        {
+            GameObject child = originalGameObject.transform.GetChild(i).gameObject;
+            puzzlePieces.Add(child);
+            pos.Add(child.transform.position);
+        }
+
+        // RESET MODEL INI
+        posModel = model.transform.position;
+
+        // LISTENERS
+        resetPuzzle.onClick.AddListener(() => {
+            int i = 0;
+            foreach(GameObject p in puzzlePieces)
+            {
+                p.transform.position = pos[i];
+                i++;
+            }
+        });
+        test.onClick.AddListener(()=>{
+            model.transform.position = posModel;
+        });
+    }
+
+}
